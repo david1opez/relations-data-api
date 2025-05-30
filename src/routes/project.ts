982c0cd22813e94eb5f04a3cdd7c7fd7a9e4936c
Diego Rdz <a00835032@tec.mx>
2025-05-30 02:11:10 -0600

    fix project

--- src/routes/project.ts
+++ src/routes/project.ts
@@ -6,11 +6,11 @@ const router = Router();
 const projectHandler = new ProjectHandler();
 
 router.get("/projects", projectHandler.getAllProjects.bind(projectHandler));
+router.get("/projects/members-count", projectHandler.getMembersCount.bind(projectHandler));
 router.get("/user-projects", projectHandler.getUserProjects.bind(projectHandler));
 router.get("/projects/:id", projectHandler.getProjectById.bind(projectHandler));
 router.post("/projects", projectHandler.addProject.bind(projectHandler));
 router.delete("/projects/:id", projectHandler.deleteProject.bind(projectHandler));
 router.patch("/projects/:id/users", projectHandler.updateProjectUsers.bind(projectHandler));
-router.get("/projects/members-count", projectHandler.getMembersCount.bind(projectHandler));
 
 export default router; 
